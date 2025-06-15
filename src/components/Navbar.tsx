d573e012039b1211257c2376a264285f48ebf226
ianlee <ethian9227@gmail.com>
2025-06-15 21:20:54 +0900

    feat: Navbar 컴포넌트 추가

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, Button, Link as ChakraLink } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
+
+interface NavbarProps {
+  user: any;
+  setUser: (user: any) => void;
+}
+
+const Navbar = ({ user, setUser }: NavbarProps) => {
+  return (
+    <Box bg="white" px={4} shadow="sm">
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center" gap={8}>
+          <ChakraLink as={RouterLink} to="/" fontWeight="bold" fontSize="xl">
+            Real Item Game
+          </ChakraLink>
+          <Flex gap={4}>
+            <ChakraLink as={RouterLink} to="/shop">
+              Shop
+            </ChakraLink>
+            <ChakraLink as={RouterLink} to="/inventory">
+              Inventory
+            </ChakraLink>
+          </Flex>
+        </Flex>
+        <Flex alignItems="center" gap={4}>
+          {user ? (
+            <>
+              <Box>Welcome, {user.email}</Box>
+              <Button onClick={() => setUser(null)}>Logout</Button>
+            </>
+          ) : (
+            <Button as={RouterLink} to="/login" colorScheme="blue">
+              Login
+            </Button>
+          )}
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Navbar 
